4bcb6b0b16f59f583c227fce7d1b30026227d69a
ImanDevback <231675761+ImandevBack@users.noreply.github.com>
2025-10-28 05:29:38 -0300

    Test

---
+++ MakeenCo-Work.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using MakeenCo_Work.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeenCo_Work.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User, Role, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
+        public DbSet<User>users { get; set; }
+        public DbSet<Role>roles { get; set; }
+        public DbSet<FAQ>fAQs { get; set; }
+        public DbSet<BlogPost>blogPosts { get; set; }
+        public DbSet<Content> content { get; set; }
+        public DbSet<DiscountCode> discountCodes { get; set; }
+        public DbSet<Regulation> regulation { get; set; }   
+        public DbSet<Reservation> reservation { get; set; }
+        
+
+
+
+
+    }
+}
