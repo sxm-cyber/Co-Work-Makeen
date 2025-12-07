1a726ced41839e164ec25e78315ebdb73a06b9aa
saman <samanafzali075@gmail.com>
2025-12-07 19:43:22 +0330

    developing unitofwork

--- MakeenCo-Work.Domain/IUnitOfWork/IUnitOfWork.cs
+++ MakeenCo-Work.Domain/IUnitOfWork/IUnitOfWork.cs
@@ -1,14 +1,30 @@
 ﻿using MakeenCo_Work.Domain.IRepository;
 using MakeenCo_Work.Domain.Repositories;
-using System.Threading.Tasks;
 
 namespace MakeenCo_Work.Application.Interfaces
 {
     public interface IUnitOfWork
     {
+        IUserRepository Users { get; }
+
+        IFaqRepository Faqs {get;}
+
+        IRegulationRepository Regulations { get; }
+
+        IWaysOfCommunicationRepository WaysOfCommunications { get; }
+
+        IDiscountCodeRepository DiscountCodes { get; }
+
+        ITieredDiscountRepository TieredDiscounts { get; }
+
+        IMessageRepository Messages { get; }
+
         IReservationRepository Reservations { get; }
+
         ISpaceRepository Spaces { get; }
+
         IBlogPostRepository BlogPosts { get; }
+
         Task<int> CompleteAsync();
     }
 }
--- MakeenCo-Work.Infrastructure/UnitOfWork/UnitOfwork.cs
+++ MakeenCo-Work.Infrastructure/UnitOfWork/UnitOfwork.cs
@@ -4,27 +4,62 @@ using MakeenCo_Work.Domain.Repositories;
 using MakeenCo_Work.Infrastructure.Data;
 using MakeenCo_Work.Infrastructure.Repositories;
 using MakeenCo_Work.Infrastructure.Repository;
+using Microsoft.Extensions.Configuration;
 
 namespace MakeenCo_Work.Infrastructure.UnitOfWork
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly ApplicationDbContext _Context;
+
+
+        public IUserRepository Users { get; }
+
+
+        public IFaqRepository Faqs { get; }
+
+        public IRegulationRepository Regulations { get; }
+
+        public IWaysOfCommunicationRepository WaysOfCommunications { get; }
+
+        public IDiscountCodeRepository DiscountCodes { get; }
+
+        public ITieredDiscountRepository TieredDiscounts { get; }
+
+        public IMessageRepository Messages { get; }
+
         public IReservationRepository Reservations { get; }
+
         public ISpaceRepository Spaces { get; }
-       
 
-        public IBlogPostRepository BlogPosts {  get; }
+        public IBlogPostRepository BlogPosts { get; }
 
-        public UnitOfWork(ApplicationDbContext context)
+        public UnitOfWork(ApplicationDbContext context , IConfiguration configuration)
         {
             _Context = context;
+
+
+            Users = new UserRepository(context, null, configuration);
+
+            Faqs = new FaqRepository(context);
+
+            Regulations = new RegulationRepository(context);
+
+            WaysOfCommunications = new WaysOfCommunicationRepository(context);
+
+            DiscountCodes = new DiscountCodeRepository(context, configuration);
+
+            TieredDiscounts = new TieredDiscountRepository(context , configuration);
+
+            Messages = new MessageRepository(context, configuration);
+
             Reservations = new ReservationRepository(context);
+
             Spaces = new SpaceRepository(context);
+
             BlogPosts =new BlogPostRepository(context);
         }
 
- 
 
         public async Task<int> CompleteAsync()
         {
